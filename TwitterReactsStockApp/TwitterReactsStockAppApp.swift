c88d9fd09c983ed8c93876a53515bacd657169f6
Ishaan Goel <goelishaan2007@gmail.com>
2024-01-01 14:50:30 -0800

    Initial Commit

---
+++ TwitterReactsStockApp/TwitterReactsStockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterReactsStockAppApp.swift
+//  TwitterReactsStockApp
+//
+//  Created by Ishaan Goel on 1/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TwitterReactsStockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
